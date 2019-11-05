437ef82161ba406a5156af2abb9473aedbd10a42
Sean George <seangeorge0@gmail.com>
2019-11-04 20:03:42 -0700

    Initial

---
+++ __main__.py
@@ -0,0 +1,11 @@
+from movie_server import db
+db.init_db()
+# cognito.UserPool(
+# resource_name = 'movie_server-users'
+# )
+# dynamodb.
+# # Create an AWS resource (S3 Bucket)
+# bucket = s3.Bucket('my-bucket')
+#
+# # Export the name of the bucket
+# pulumi.export('bucket_name', bucket.id)
